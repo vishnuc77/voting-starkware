94ec9215876006cd2d5e6cf138356c55c9c83966
Vishnu C <vishnuc@Vishnus-MacBook-Air.local>
2021-12-03 23:27:11 +0530

    tweaks front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,21 @@ import './App.css';
 import { stark, compileCalldata } from 'starknet';
 
 
-const VOTER_ADDRESS ="0x02048f29f9b97e9526077478925f569da4215daa6dd9329c033f29df08f65228";
-const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888cb7d0f3e32";
+const VOTER_ADDRESS ="0x00e5bf6520f82336d78f1082550f58cf037efa6e01fa6e8b66384af68720b083";
+const RESULT_ADDRESS ="0x022ac0cd7c8c00d0202c3f2bc7e209421b253d963bca7d451ba5d63b80313a50";
 
   function App() {
     const [ pollId, setPollId ] = useState(0);
     const [ voteIndex, setVoteIndex ] = useState(0);
-    const [ tx, setTx ] = useState(0);
     const [ voterAddress, setVoterAddress ] = useState(0);
     const [ txHash, setTxHash ] = useState(0);
     const [ visibility, setVisibility ] = useState(false);
-    const [ txStatus, setTxStatus ] = useState("");
+    const [ txStatus, setTxStatus ] = useState('');
     const [ txStatusVisibility, setTxStatusVisibility ] = useState(false);
+    const [ votingState, setVotingState ] = useState();
+    const [ votingStateVisibility, setVotingStateVisibility] = useState(false);
+    const [ result, setResult ] = useState('');
+    const [ resultVisibility, setResultVisibility ] = useState(false);
 
     async function initiate () {
       if (!pollId) return
@@ -38,7 +41,9 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
       console.log(initPollResponse);
       console.log(initPollResponse.transaction_hash);
       setVisibility(true);
-      setTxHash(initPollResponse.transaction_hash)
+      setTxHash(initPollResponse.transaction_hash);
+      setResultVisibility(false);
+      getTransactionStatus(initPollResponse.transaction_hash);
     }
 
     async function registerVoter () {
@@ -62,6 +67,7 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
       console.log(registerVoterResponse.transaction_hash);
       setVisibility(true);
       setTxHash(registerVoterResponse.transaction_hash);
+      getTransactionStatus(registerVoterResponse.transaction_hash);
     }
 
     async function vote () {
@@ -85,6 +91,7 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
       console.log(voteResponse.transaction_hash);
       setVisibility(true);
       setTxHash(voteResponse.transaction_hash);
+      getTransactionStatus(voteResponse.transaction_hash);
     }
 
     async function getVotingState () {
@@ -106,6 +113,8 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
       });
       console.log(getVotingStateResponse.result[0]);
       console.log(getVotingStateResponse.result[1]);
+      setVotingState(getVotingStateResponse.result);
+      setVotingStateVisibility(true);
     }
 
     async function finalize () {
@@ -128,6 +137,7 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
       console.log(finalizeResponse.transaction_hash);
       setVisibility(true);
       setTxHash(finalizeResponse.transaction_hash);
+      getTransactionStatus(finalizeResponse.transaction_hash);
     }
 
     async function getResult () {
@@ -147,29 +157,39 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
         entry_point_selector: getResultSelector,
         calldata: callDataHash,
       });
-      console.log(getResultResponse);
+      console.log(parseInt(getResultResponse.result[0])); 
+      if(parseInt(parseInt(getResultResponse.result[0])) === 1)
+        setResult('Yes');
+      else
+        setResult('No');
+      setResultVisibility(true);
     }
 
-    async function getTransactionStatus () {
-      setVisibility(false);
-      if (!tx) return
+    async function getTransactionStatus (tx) {
+      //setVisibility(false);
+      //if (!tx) return
       const starknet = getStarknet();
       await starknet.enable()
       if (starknet.isConnected === false)
         throw Error("starknet wallet not connected")
 
-      const status = await starknet.provider.getTransactionStatus(tx);
-      console.log(status.tx_status);
-      setTxStatusVisibility(true);
-      setTxStatus(status.tx_status);
+      while(txStatus !== 'ACCEPTED_ONCHAIN' || txStatus !== 'REJECTED') {
+        const status = await starknet.provider.getTransactionStatus(tx);
+        console.log(status.tx_status);
+        setTxStatusVisibility(true);
+        setTxStatus(status.tx_status);
+        if(status.tx_status === "ACCEPTED_ONCHAIN" || status.tx_status === "REJECTED")
+          break;
+      }
     }
     
     return (
       <div className="App">
         <div className="topnav">
-            <a><strong>VOTING DAPP STARKWARE</strong></a>
+            <a><strong>VOTING DAPP - STARKWARE</strong></a>
         </div>
-        <header className="App-header">
+        <header className="App-header"> <br />
+          <center>* Please don't send new transactions until last transaction status becomes ACCEPTED_ONCHAIN</center><br />
           <input type="text" onChange={e => setPollId(e.target.value)} placeholder="Poll ID" />&nbsp;
           <button className="button" onClick={initiate}>Initiate Poll</button><br/>
           <input type="text" onChange={e => setPollId(e.target.value)} placeholder="Poll ID" />&nbsp;
@@ -180,14 +200,16 @@ const RESULT_ADDRESS ="0x05ff5e16caf8a8be54655be16f4560d37a6986451376dcea236888c
           <button className="button" onClick={vote}>Vote</button><br/>
           <input type="text" onChange={e => setPollId(e.target.value)} placeholder="Poll ID" />&nbsp;
           <button className="button" onClick={getVotingState}>Get Voting State</button><br/>
+          { votingStateVisibility && <p>No votes: {parseInt(votingState[0])} &nbsp;&nbsp;&nbsp; Yes Votes: {parseInt(votingState[1])}</p> }
           <input type="text" onChange={e => setPollId(e.target.value)} placeholder="Poll ID" />&nbsp;
           <button className="button" onClick={finalize}>Finalize poll</button><br/>
           <input type="text" onChange={e => setPollId(e.target.value)} placeholder="Poll ID" />&nbsp;
           <button className="button" onClick={getResult}>Get result</button><br/><br/>
+          { resultVisibility && <p>Result of pollID {pollId}: {result}</p> }
           { visibility && <p>Transaction hash: {txHash}</p> }
-          <input type="text" onChange={e => setTx(e.target.value)} placeholder="TX hash" />&nbsp;
-          <button className="button" onClick={getTransactionStatus}>Get transaction status</button>
-          { txStatusVisibility && <p>Transaction status: {txStatus}</p> }
+          {/* <input type="text" onChange={e => setTx(e.target.value)} placeholder="TX hash" />&nbsp;
+          <button className="button" onClick={getTransactionStatus}>Get transaction status</button> */}
+          { txStatusVisibility && <p>Transaction status: <b>{txStatus}</b></p> }
         </header>
       </div>
     );
